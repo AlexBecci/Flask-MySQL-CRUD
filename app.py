3fbe1dbc31b6eb6ffcde57fe5ba00014a6003d6e
Alex Becci <alex.becci@bingopilar.corp>
2024-01-11 15:15:24 -0300

    Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from routes.contacts import contacts
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:1234becci@localhost/contactsdb"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+SQLAlchemy(app)
 
 app.register_blueprint(contacts)
